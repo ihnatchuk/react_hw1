7761640e6025af3e1ad1376ed17b79b71f832064
ihnatchuk <ihnatchukpp@gmail.com>
2023-01-25 01:44:45 +0200

    hw1 v1.2.1 axios key added

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
       <div className="App">
         {
-          characters.map( person => <Character person={person} /> )
+          characters.map( person => <Character key={person.id} person={person} /> )
         }
       </div>
   );
